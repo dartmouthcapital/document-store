8567b7617a6860b1204077d76a4d3c07309a175b
Todd Mannherz <todd.mannherz@gmail.com>
2017-04-14 16:46:16 -0400

    Logging of server messages.

--- bin/server.dart
+++ bin/server.dart
@@ -11,23 +11,41 @@ import '../lib/router.dart' as app;
 
 main(List<String> args) async {
     await Config.ready();
-    var logHandler = new SyncFileLoggingHandler('var/request.log');
+    var logHandler = new SyncFileLoggingHandler('var/server.log');
 
     Handler handler = const Pipeline()
         .addMiddleware(exceptionHandler())
         .addMiddleware(logRequests(logger: (String msg, bool isError) {
-            LogRecord record = new LogRecord(Level.INFO, msg, 'test', isError);
+            LogRecord record = new LogRecord(Level.INFO, msg, 'request', isError);
             logHandler.call(record);
         }))
         .addMiddleware(logRequests())
         .addMiddleware(app.appMiddleware)
         .addHandler(app.appRouter.handler);
 
-    printRoutes(app.appRouter);
+    printRoutes(app.appRouter, printer: (printStr) {
+        print(printStr);
+        LogRecord record = new LogRecord(Level.INFO, printStr, 'server');
+        logHandler.call(record);
+    });
 
     assert(Config.get('server/host') is String);
     assert(Config.get('server/port') is int);
-    io.serve(handler, Config.get('server/host'), Config.get('server/port')).then((server) {
-        print('Serving at http://${server.address.host}:${server.port}');
-    }).catchError((error) => print(error));
+    io.serve(handler, Config.get('server/host'), Config.get('server/port'))
+        .then((server) {
+            String msg = 'Serving at http://${server.address.host}:${server.port}';
+            print(msg);
+            LogRecord record = new LogRecord(Level.INFO, msg, 'server');
+            logHandler.call(record);
+        }).catchError((error, stackTrace) {
+            print(error);
+            LogRecord record = new LogRecord(
+                Level.SEVERE,
+                error.message,
+                'server',
+                true,
+                stackTrace
+            );
+            logHandler.call(record);
+        });
 }
